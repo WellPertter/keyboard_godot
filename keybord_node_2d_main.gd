a642f30e385c4c74ab8e7c2c97b134db2788ebf0
Lopes <josearthur.12311@gmail.com>
2024-10-20 16:13:21 -0300

    Update the game, add the style and the songs, update the script

--- keybord_node_2d_main.gd
+++ keybord_node_2d_main.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 var pontos = 0
+var erros = 0
+var dentativas = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	generationClick()
 	pass # Replace with function body.
 
 func _input(event) -> void:
@@ -25,34 +28,66 @@ func _input(event) -> void:
 				_on_buttone_d_pressed()
 			pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	$NodeMain/LblScore.text = 'Score ' + str(pontos)
+	if pontos == 97:
+		get_tree().change_scene_to_file("res://creenwin.tscn")
 	pass
-
-
+	
+	
+func generationClick():
+	$NodeMain/ButtoneD.modulate = Color("#FFFFFF")
+	$NodeMain/ButtoneA.modulate = Color("#FFFFFF")
+	$NodeMain/ButtoneW.modulate = Color("#FFFFFF")
+	$NodeMain/ButtoneS.modulate = Color("#FFFFFF")
+	
+	var random_number = randi_range(1, 4)
+	
+	if random_number == 1:
+		$NodeMain/ButtoneD.modulate = Color("#00FF00")
+	if random_number == 2:
+		$NodeMain/ButtoneA.modulate = Color("#00FF00")
+	if random_number == 3:
+		$NodeMain/ButtoneW.modulate = Color("#00FF00")
+	if random_number == 4:
+		$NodeMain/ButtoneS.modulate = Color("#00FF00")
+	pass
+	
 func _on_buttone_esc_pressed() -> void:
 	get_tree().quit()
 	pass # Replace with function body.
 
+func verificador(botao: Button):
+	if botao.modulate ==  Color("#00FF00"):
+		acerto()
+	else:
+		erro()
+	pass
 
-func _on_buttone_s_pressed() -> void:
+func acerto():
+	dentativas += 1
 	pontos += 1
-	print(str(pontos))
+	$NodeMain/bgMusicAcerto.playing = true
+	generationClick()
+	
+func erro():
+	dentativas += 1
+	pontos -= 19
+	erros += 1
+	$NodeMain/bgMusicErro.playing = true
+	
+func _on_buttone_s_pressed() -> void:
+	verificador($NodeMain/ButtoneS)
 	pass # Replace with function body.
-
 
 func _on_buttone_w_pressed() -> void:
+	verificador($NodeMain/ButtoneW)
 	pass # Replace with function body.
-
 
 func _on_buttone_a_pressed() -> void:
+	verificador($NodeMain/ButtoneA)
 	pass # Replace with function body.
-
 
 func _on_buttone_d_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_lbl_score_ready() -> void:
+	verificador($NodeMain/ButtoneD)
 	pass # Replace with function body.
